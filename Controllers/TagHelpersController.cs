ffd598ec4142a9193812e0bd0417d82a73a899d3
JDBrothers1016 <jp1016charusat@gmail.com>
2022-03-03 11:39:24 +0530

    Added Tag helpers into it

---
+++ Controllers/TagHelpersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDOperation.Controllers
+{
+    public class TagHelpersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
